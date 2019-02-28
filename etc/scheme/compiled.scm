c09f650cb5866076adb2480be4212fb117d7f034
Andrey Cizov <acizov@gmail.com>
2019-02-28 14:04:46 +0000

    working on the compiled structures

---
+++ etc/scheme/compiled.scm
@@ -0,0 +1,16 @@
+#| every closure is a lambda |#
+
+(define
+  closure_234656
+  (closure
+    (a b c) (e f g)
+    (body 5)))
+
+(define
+  closure_23456
+  (closure
+    (i y z) (e f h)
+    (3)))
+
+#| define an operation that is directly compiled to an op |#
+(define (nanop musings ()))
